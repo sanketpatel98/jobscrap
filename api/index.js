03ed57fec7761741907975c8b95bf0588b144f27
sanketpatel <patelsanketr98@gmail.com>
2024-09-11 16:08:01 -0400

    Scrap form added

--- api/index.js
+++ api/index.js
@@ -1,10 +1,19 @@
-import bodyParser from "body-parser";
-import cors from "cors";
-import express from "express";
-import scrapJob from "../service/scrapFromDescription.js";
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import scrapJob from '../service/scrapFromDescription.js';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 const app = express();
 const port = 3000;
+
+// Resolve __dirname equivalent
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Serve static files from the 'public' directory, which is at the same level as 'api'
+app.use(express.static(join(__dirname, '../public')));
 
 // Use CORS middleware
 app.use(cors());
@@ -13,23 +22,18 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Define a POST endpoint for scraping
-app.post("/scrap", async (req, res) => {
+app.post('/scrap', async (req, res) => {
   try {
     const dataToReturn = await scrapJob(req.body); // Wait for scrapJob to complete
     res.status(200).send(dataToReturn);
   } catch (error) {
-    res.status(500).send("An error occurred while processing your request."); // Respond with an error status code
+    res.status(500).send('An error occurred while processing your request.'); // Respond with an error status code
   }
 });
 
 // Serve the HTML file on the root URL
-app.get("/", async (req, res) => {
-  try {
-    await scrapJob(req.body); // Wait for scrapJob to complete
-    res.status(200).send("Data received successfully!");
-  } catch (error) {
-    res.status(500).send("An error occurred while processing your request."); // Respond with an error status code
-  }
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, '../public', 'index.html'));
 });
 
 app.listen(port, () => {
