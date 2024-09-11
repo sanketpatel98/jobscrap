55497ec01b217fbab800de15b77b44d67b53159b
sanketpatel <patelsanketr98@gmail.com>
2024-09-11 15:42:53 -0400

    job level types defined

--- service/scrapFromDescription.js
+++ service/scrapFromDescription.js
@@ -9,7 +9,7 @@ const scrapJob = (data) => {
         { name: "company", isArray: false },
     ];
     const topic =
-        "I want you extract some specific data from a large chunck of data. I will provide you job related webpge data which can be in very bad format. I want you to get me only 4 details from it. Do it for the first company only";
+        "I want you extract some specific data from a large chunck of data. I will provide you job related webpge data which can be in very bad format. I want you to get me only 4 details from it. Do it for the first company only. PS: Make sure the data for the company parameter must be from the following list(it is your responsibility to select one according to data given): 'Associate','Entry level','Intern', 'Mid-Senior level', 'NA', 'Volunteer', 'Temporary'  ";
 
     const finalData = topic + data.jd;
 
